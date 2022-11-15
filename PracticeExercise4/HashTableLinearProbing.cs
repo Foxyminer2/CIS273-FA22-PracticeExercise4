a84161b670664ab786dc1793f2c5b4aa2ac9423d
Aapple21 <aapplegate12@icloud.com>
2022-11-15 00:57:24 -0600

    Update HashTableLinearProbing.cs

--- PracticeExercise4/HashTableLinearProbing.cs
+++ PracticeExercise4/HashTableLinearProbing.cs
@@ -19,16 +19,55 @@ namespace PracticeExercise4
             }
 
 		}
+        private int count;
+        public int Count => count;
 
-        public int Count => throw new NotImplementedException();
-
-        public double LoadFactor => throw new NotImplementedException();
+        private readonly double MAX_LOAD_FACTOR = 0.6;
+        public double LoadFactor => count / (double)buckets.Length;
 
         public bool Add(K key, V value)
         {
-            throw new NotImplementedException();
+
+            if(LoadFactor > MAX_LOAD_FACTOR)
+            {
+                Resize();
+            }
+            //if hashtable add else dont
+
+            //find hash
+            int hash = Hash(key);
+
+            //find starting index
+            int startingIndex = hash % buckets.Length;
+            int bucketIndex = startingIndex;
+
+            //if key already exists, then update it
+            while (buckets[bucketIndex].State == BucketState.Full)
+            {
+                //if the key already exists, then update it
+                if (buckets[bucketIndex].Key.Equals(key))
+                {
+                    buckets[bucketIndex].Value = value;
+                    return true;
+                }
+
+                bucketIndex = (bucketIndex + 1) % buckets.Length;
+
+                if (bucketIndex == startingIndex)
+                {
+                    throw new OutOfMemoryException();
+                }
+            }
+
+            //if key doesnt exists, add it
+            buckets[bucketIndex].Key = key;
+            buckets[bucketIndex].Value = value;
+            buckets[bucketIndex].State = BucketState.Full;
+            count++;
+            return false;
         }
 
+        // O(?)
         public bool ContainsKey(K key)
         {
             throw new NotImplementedException();
@@ -57,6 +96,36 @@ namespace PracticeExercise4
         public bool Remove(K key)
         {
             throw new NotImplementedException();
+        }
+
+        private void Resize()
+        {
+            var newBuckets = new Bucket<K, V>[2 * buckets.Length];
+            var oldBuckets = buckets;
+
+            buckets = newBuckets;
+            for(int i=0; i < buckets.Length; i++)
+            {
+                buckets[i] = new Bucket<K, V>();
+            }
+
+            count = 0;
+
+            //rehash all the old/existing buckets into the new array/hashtable
+            foreach (var bucket in oldBuckets)
+            {
+                if (bucket.State == BucketState.Full)
+                {
+                    Add(bucket.Key, bucket.Value);
+                }
+            }
+        }
+
+        private int Hash(K key)
+        {
+            int hash = key.GetHashCode();
+
+            return hash < 0 ? -hash : hash;
         }
     }
 }
